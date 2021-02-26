80bdafebff9e241f76f9c1cbcace9ac0db0c5616
benchan777 <benchan777@gmail.com>
2021-02-25 18:27:45 -0800

    add authentication routes

--- recipe_tracker_app/authentication/routes.py
+++ recipe_tracker_app/authentication/routes.py
@@ -13,3 +13,34 @@ def load_user(user_id):
 @authentication.route('/signup', methods = ['GET', 'POST'])
 def signup():
     form = Signup()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(
+            username = form.username.data,
+            password = hashed_password
+        )
+        db_sql.session.add(user)
+        db_sql.session.commit()
+
+        flash('Account created!')
+        return redirect(url_for('authentication.login'))
+    return render_template('signup.html', form = form)
+
+@authentication.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = Login()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember = True)
+            next_page = request.args.get('next')
+            return redirect(next_page if next_page else url_for('main.homepage'))
+
+    return render_template('login.html', form = form)
+
+@authentication.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.homepage'))
