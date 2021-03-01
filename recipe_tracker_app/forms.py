7119d00d0f29bfdc9d0bf2b2d2aebdf0583f6ce2
benchan777 <benchan777@gmail.com>
2021-02-28 17:59:49 -0800

    change category into selectfield

--- recipe_tracker_app/forms.py
+++ recipe_tracker_app/forms.py
@@ -1,16 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField
+from wtforms import StringField, PasswordField, SubmitField, SelectField, TextAreaField
 from wtforms.validators import DataRequired, Length, ValidationError
-from recipe_tracker_app.models import User, Recipe, Ingredients
+# from recipe_tracker_app.models import User, Recipe, Ingredients
 
 class RecipeForm(FlaskForm):
-    # category = SelectField('Category', choices = [('DESSERT', 'Dessert'), ('ENTREES', 'Entrees'), ('APPETIZERS', 'Appetizers'), ('OTHER', 'Other')])
-    category = StringField('Category', validators = [DataRequired()])
-    instructions = StringField('Instructions', validators = [DataRequired()])
+    name = StringField('Name', validators = [DataRequired()])
+    category = SelectField('Category', choices = [('DESSERT', 'Dessert'), ('ENTREES', 'Entrees'), ('APPETIZERS', 'Appetizers'), ('OTHER', 'Other')])
+    instructions = TextAreaField('Instructions', validators = [DataRequired()])
     submit = SubmitField('Submit Recipe')
 
 class IngredientsForm(FlaskForm):
-    # category = SelectField('Category', choices = [('VEGETABLE', 'Vegetable'), ('MEAT', 'Meat'), ('DAIRY', 'Dairy'), ('GRAINS', 'Grains'), ('FRUITS', 'Fruits'), ('OILS', 'Oils'), ('FISH', 'Fish'), ('OTHER', 'Other')])
-    category = StringField('Category', validators = [DataRequired()])
+    category = SelectField('Category', choices = [('VEGETABLES', 'Vegetable'), ('MEAT', 'Meat'), ('DAIRY', 'Dairy'), ('GRAINS', 'Grains'), ('FRUITS', 'Fruits'), ('OILS', 'Oils'), ('FISH', 'Fish'), ('OTHER', 'Other')])
     name = StringField('Name', validators = [DataRequired()])
     submit = SubmitField('Submit Ingredient')
