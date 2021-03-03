fc44b28329bc06db4630c775744906037a197441
benchan777 <benchan777@gmail.com>
2021-03-02 22:57:35 -0800

    add username and password validation

--- recipe_tracker_app/authentication/forms.py
+++ recipe_tracker_app/authentication/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, ValidationError
 from recipe_tracker_app.models import User
+from recipe_tracker_app import bcrypt
 
 class Signup(FlaskForm):
     username = StringField('Username', validators = [DataRequired(), Length(min = 3, max = 40)])
@@ -17,3 +18,15 @@ class Login(FlaskForm):
     username = StringField('Username', validators = [DataRequired(), Length(min = 3, max = 40)])
     password = PasswordField('Password', validators = [DataRequired(), Length(min = 3)])
     submit = SubmitField('Log In')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username.data).first()
+
+        if not user:
+            raise ValidationError('That username does not exist. Please try again.')
+
+    def validate_password(self, password):
+        user = User.query.filter_by(username = self.username.data).first()
+
+        if user and not bcrypt.check_password_hash(user.password, password.data):
+            raise ValidationError('Incorrect password. Please try again.')
