6b8a98562268ccd924c5eeaaa1a5c5cfcad3f8ef
benchan777 <benchan777@gmail.com>
2021-03-02 22:58:34 -0800

    add tests for login, logging in w/ wrong password, logout, logging in with nonexistent username

--- recipe_tracker_app/authentication/tests.py
+++ recipe_tracker_app/authentication/tests.py
@@ -1,16 +1,16 @@
-from unittest import TestCase
+import unittest
 from recipe_tracker_app import app, bcrypt, db
 from recipe_tracker_app.models import User, Recipe, Ingredients
 
 # Setup
 def new_user():
-    user = User(username = 'ben', password = password)
     password = bcrypt.generate_password_hash('password')
+    user = User(username = 'ben', password = password)
     db.session.add(user)
     db.session.commit()
 
 # Tests
-class AuthTests(TestCase):
+class AuthTests(unittest.TestCase):
     """Tests for authentication (login & signup)."""
 
     def setUp(self):
@@ -31,4 +31,57 @@ class AuthTests(TestCase):
         }
         self.app.post('/signup', data = user_data)
 
-        
+        user = User.query.filter_by(username = 'test_user').one()
+        self.assertIsNotNone(user)
+
+    def login_test(self):
+        ''' Test login route '''
+        new_user()
+
+        data = {
+            'username': 'ben',
+            'password': 'password'
+        }
+        self.app.post('/login', data = data)
+
+        response = self.app.get('/', follow_redirects = True)
+        self.assertIn('Log Out', response.get_data(as_text = True))
+
+
+    def login_wrong_pw(self):
+        ''' Test logging in with incorrect password '''
+        new_user()
+
+        data = {
+            'username': 'ben',
+            'password': 'wrong_password'
+        }
+
+        response = self.app.post('/login', data = data)
+        self.assertIn('Incorrect password. Please try again.', response.get_data(as_text = True))
+
+    def login_nonexistent_username(self):
+        ''' Test logging in with nonexistent username '''
+        new_user()
+
+        data = {
+            'username': 'jaldhfjaafj',
+            'password': 'password'
+        }
+
+        response = self.app.post('/login', data = data)
+        self.assertIn('That username does not exist. Please try again.', response.get_data(as_text = True))
+
+    def logout_test(self):
+        ''' Test logging out '''
+        new_user()
+
+        data = {
+            'username': 'ben',
+            'password': 'password'
+        }
+
+        self.app.post('/login', data = data)
+
+        response = self.app.get('/logout', follow_redirects = True)
+        self.assertNotIn('Log Out', response.get_data(as_text = True))
